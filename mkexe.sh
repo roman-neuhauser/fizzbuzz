8461af9141947f9598d8712f3bafe1e7b0bf6c2b
Roman Neuhauser <neuhauser@sigpipe.cz>
2016-07-11 10:12:15 +0200

    mkexe safer against failed configure checks

--- mkexe.sh
+++ mkexe.sh
@@ -3,6 +3,6 @@ target=$2
 interp=$3
 
 mkdir -p $(dirname $target)
-sed -e '1s@#![^[:space:]]*@#!'"$interp@" < $source > $target.tmp
+sed -e "1s^#![^[:space:]]*^#!$interp^" < $source > $target.tmp
 install -m 0755 $target.tmp $target
 rm -f $target.tmp
